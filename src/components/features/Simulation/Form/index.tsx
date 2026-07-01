dc622db2489b0bf067da506a5d81014e09e6b9d6
Evelin Une <prixlin@gmail.com>
2026-07-01 10:13:37 -0300

    feat: formSteps components and logic states

--- src/components/features/Simulation/Form/index.tsx
+++ src/components/features/Simulation/Form/index.tsx
@@ -1,3 +1,40 @@
-export function SimulationForm() {
-	return <div>Form de simulação</div>
+import { FormStep } from '@/components/features/Simulation/FormStep'
+import { StepProgress } from '@/components/features/Simulation/Progress'
+import { simulationFormSteps } from '@/data/simulation'
+import { useState } from 'react'
+
+export const SimulationForm = () => {
+	const [currentStepIndex, setCurrentStepIndex] = useState(0)
+	const totalSteps = simulationFormSteps.length
+	const currentStep = simulationFormSteps[currentStepIndex]
+
+	const handleNextStep = () => {
+		if (currentStepIndex + 1 > totalSteps - 1) {
+			return
+		}
+
+		setCurrentStepIndex((prev) => prev + 1)
+	}
+
+	const handlePreviousStep = () => {
+		if (currentStepIndex === 0) {
+			return
+		}
+		setCurrentStepIndex((prev) => prev - 1)
+	}
+
+	return (
+		<>
+			<StepProgress
+				currentStep={currentStepIndex + 1}
+				totalSteps={totalSteps}
+			/>
+			<FormStep
+				key={currentStep.id}
+				{...currentStep}
+				onBack={handlePreviousStep}
+				onNext={handleNextStep}
+			/>
+		</>
+	)
 }
